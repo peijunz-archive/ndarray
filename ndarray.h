60c723d6a9182a3c4720a5028b91b7976b3459ce
Peijun Zhu <zpj.ustc@gmail.com>
2018-10-05 17:59:42 -0400

    simplify

--- ndarray.h
+++ ndarray.h
@@ -19,57 +19,28 @@ class ndarray {
     using size_t=int32_t;
     using iterator=dtype *;
     using const_iterator=const dtype *;
+
     ~ndarray() {
         delete [] head;
     }
-    /// Constructor an empty ndarray
+
     ndarray(): head(nullptr) {}
-    inline void init_strides(){
-        _stride[D] = 1;
-        for(int i = 0; i < D; i++)
-            _stride[D - i - 1] = _stride[D - i] * _shape[D - i - 1];
-    }
-    /**
-     * @brief Initialize an ndarray for a square matrix
-     * @param width
-     */
+
     explicit ndarray(size_t width){
-        for(int i = 0; i < D; i++) {
-            _shape[i] = width;
-        }
-        check();
-        init_strides();
-        head = new dtype[size()];
-    }
-    /**
-     * @brief A ndarray with a shape given by pointer/array
-     * @param sh    shape array
-     *
-     * For dynamic construction
-     */
-    explicit ndarray(const size_t *sh){
-        copy(sh, sh+D, _shape);
-        check();
-        init_strides();
-        head = new dtype[size()];
-    }
-    /**
-     * @brief Initialize an ndarray with a shape given by an initializer list
-     * @param l     initializer list for shape
-     *
-     * For static construction
-     */
-    explicit ndarray(initializer_list<size_t> l){
-        copy(l.begin(), l.end(), _shape);
-        check();
-        init_strides();
-        head = new dtype[size()];
+        fill(_shape, _shape+D, width);
+        allocate_data();
     }
 
-    /**
-     * @brief Copy constructor copying the shape only.
-     * @param x     The source of ndarray
-     */
+    explicit ndarray(const size_t *sh){
+        copy(sh, sh+D, _shape);
+        allocate_data();
+    }
+
+    explicit ndarray(initializer_list<size_t> l){
+        copy(l.begin(), l.end(), _shape);
+        allocate_data();
+    }
+
     template<typename T>
     ndarray(const ndarray<T, D> & x){
         copy(x._shape, x._shape+D, _shape);
@@ -77,16 +48,13 @@ class ndarray {
         head = new dtype[size()];
         copy(x.cbegin(), x.cend(), head);
     }
-    /// Move constructor
+
     ndarray(ndarray<dtype, D> && x):head(x.head){
         copy(x._shape, x._shape+D, _shape);
         copy(x._stride, x._stride+D+1, _stride);
         x.head = nullptr;
     }
-    /**
-     * @brief Copy Assignment copying all data
-     * @param x     The source of ndarray
-     */
+
     ndarray<dtype, D> & operator= (const ndarray<dtype, D>& x) {
         if(this != &x) {
             ndarray<dtype, D> lhs(x);
@@ -94,46 +62,20 @@ class ndarray {
         }
         return *this;
     }
-    /// Move assignment
+
     ndarray<dtype, D> & operator= (ndarray<dtype, D>&& x) {
         copy(x._shape, x._shape+D, _shape);
         copy(x._stride, x._stride+D+1, _stride);
         swap(head, x.head);
         return *this;
     }
-    /**
-     * @brief Set all the values in the array to a init value
-     * @param val   initial value
-     */
+
     ndarray<dtype, D> & operator= (dtype val) {
-        for(size_t i = 0; i < size(); i++)
-            head[i] = val;
+        fill(begin(), end(), val);
         return *this;
     }
-    size_t size() const {
-        return *_stride;
-    }
-    int dim() const {
-        return D;
-    }
-    size_t shape(int i) const {
-        return _shape[i];
-    }
-    const size_t * shape() const {
-        return _shape;
-    }
-    size_t stride(int i) const {
-        return _stride[i + 1];
-    }
-    const size_t * stride() const {
-        return _stride + 1;
-    }
 
-    /**
-     * @brief Indexing by an index array
-     * @param coo   the coordinates of an element
-     * @return The indexed element
-     */
+
     template <class IndexIterator>
     dtype & operator[](const IndexIterator coo) {
         size_t offset = 0;
@@ -152,109 +94,63 @@ class ndarray {
         offset += coo[D-1];
         return head[offset];
     }
-    /**
-     * @brief Naive indexing using raw index
-     * @param rawind    raw index
-     *
-     * + Good efficiency!
-     * + If dim==1, use [] rather than ()
-     */
+
     dtype & operator[](size_t rawind) {
-        return *(head + rawind);
+        return head[rawind];
     }
     const dtype & operator[](size_t rawind) const {
-        return *(head + rawind);
+        return head[rawind];
     }
 
-
-    size_t adder(size_t k, size_t last) const {
-        return last;//k=1
+    template<int axis>
+    size_t adder(size_t last) const {
+        return _stride[axis+1] * last;
     }
-    template<typename... Args>
-    size_t adder(size_t k, size_t first, Args... args) const {
-        return _stride[k] * first + adder(k + 1, args...);
+
+    template<int axis, typename... Args>
+    size_t adder(size_t first, Args... args) const {
+        return _stride[axis+1] * first + adder<axis+1>(args...);
     }
 
     /**
      * @brief Indexing by the argument list of operator()
      * @param args the index of an element
      *
-     * + Very useful for array with known dimension.
-     * + Emulate the original C style array
-     * + If _dim==1, use [] directly rather than ()
-     * + If args are not enough, suppose we fill it with 0
+     * + If args are not enough, we fill it with 0
      *
      * @return The indexed element
      */
     template<typename... Args>
     dtype & operator()(Args... args) {
-        return *(head + adder(1, args...));
+        return head[adder<0>(args...)];
     }
     template<typename... Args>
-    const dtype & operator()(size_t arg0, Args... args) const {
-        return *(head + adder(1, args...));
+    const dtype & operator()(Args... args) const {
+        return head[adder<0>(args...)];
     }
 
-    template<bool dir>
-    /**
-     * @brief Roll the index in a given axis with periodical boundary condition
-     * @param rawind raw index
-     * @param axis
-     * @param dir   positive/negative
-     * + For positive axis: `0, 1,..., dim-1`
-     * + For corresponding negative axis: `-dim, 1-dim,..., -1`
-     * @return Raw index after rolling
-     */
-    size_t rollindex(size_t rawind, int axis) const {
-        size_t axisind = (rawind % _stride[axis]) / _stride[axis + 1];
-        rawind += (2*dir-1)*_stride[axis + 1];
-        if(axisind == dir*(_shape[axis]-1))
-            rawind -= (2*dir-1)*_stride[axis];
-        return rawind;
-    }
-    /**
-     * @brief roll ind for unknow positiveness by using rollindex
-     */
-    size_t rollind(size_t rawind, int ax) const {
-        if(ax < 0) {
-            return rollindex<false>(rawind, ax + D);
-        } else {
-            return rollindex<true>(rawind, ax);
-        }
-    }
-
-    /// Transpose between two axis
-    void transpose(int i = 1, int j = 0) {
-        swap(_stride[i + 1], _stride[j + 1]);
-        swap(_shape[i + 1], _shape[j + 1]);
-    }
-    int size_attached() const { ///< empty condition
-        int s = sizeof(ndarray<dtype, D>);
-        s += sizeof(size_t) * (2 * D + 1);
-        return s;
-    }
     ndarray<dtype, D>& operator-() const{
         ndarray<dtype, D> tmp(*this);
         for(size_t i=0;i<size();i++){
             tmp[i]=-head[i];
         }
-        return *this;
+        return tmp;
     }
-    template<int N>
+    template<int k>
     /**
      * Adding rhs with some constexpr coefficient
      * Also foundation for plus, minus
      */
-    ndarray<dtype, D>& pm(const ndarray<dtype, D> &rhs){
+    ndarray<dtype, D>& plus_k_rhs(const ndarray<dtype, D> &rhs){
         for(size_t i = 0; i < size(); i++)
-            head[i] += N*rhs[i];
+            head[i] += k*rhs[i];
         return *this;
     }
     ndarray<dtype, D>& operator+=(const ndarray<dtype, D> &rhs){
-        return this->pm<1>(rhs);
+        return this->plus_k_rhs<1>(rhs);
     }
     ndarray<dtype, D>& operator-=(const ndarray<dtype, D> &rhs){
-        return *this->pm<-1>(rhs);
+        return *this->plus_k_rhs<-1>(rhs);
     }
     ///< Not using += and -= directly because of the copying cost
     ndarray<dtype, D>& operator+(const ndarray<dtype, D> &rhs) const {
@@ -283,43 +179,75 @@ class ndarray {
     const_iterator cend() const {
         return head+size();
     }
+    size_t size() const {return *_stride;}
+    int dim() const {return D;}
+    size_t shape(int i) const {return _shape[i];}
+    const size_t * shape() const {return _shape;}
+    size_t stride(int i) const {return _stride[i + 1];}
+    const size_t * stride() const {return _stride + 1;}
+    const size_t * raw_stride() const {return _stride ;}
   private:
-    static_assert(D > 0, "Dimension should be positive!");
+    static_assert(D > 0, "Dimension should be positive integer!");
     size_t __restrict__ _shape[D] = {0};         ///< shape of each dimension
-    /**
-     * @brief stride of every axis.
-     *
-     * For the purpose of transposition,
-     * the naive stride of last index is reserved.
-     * So the last stride may be non-zero. Be cautious!
-     */
-    size_t _stride[D+1] = {0};
+    size_t __restrict__ _stride[D+1] = {0};                  ///< stride of every axis
     dtype* __restrict__ head = nullptr;        ///< head of the array data
-    void check() {
-        for(int i = 0; i < D; i++) {
+
+    void allocate_data() {
+        _stride[D] = 1;
+        for(int i = 0; i < D; i++){
             if(_shape[i] <= 0) {
                 cerr << "Error: Positive shape needed!" << endl;
                 exit(0);
             }
+            _stride[D - i - 1] = _stride[D - i] * _shape[D - i - 1];
         }
+        head = new dtype[size()];
     }
 };
 
-template<class dtype>
+template<bool forward>
 /**
- * Two dimensional array---matrix
- */
+* @brief Roll the index in a given axis with periodical boundary condition
+* @param rawind raw index
+* @param axis
+* @param forward   positive/negative
+* + For positive axis: `0, 1,..., dim-1`
+* + For corresponding negative axis: `-dim, 1-dim,..., -1`
+* @return Raw index after rolling
+*/
+size_t rollindex(size_t rawind, int axis, const size_t* shape, const size_t* raw_stride) {
+    size_t axisind = (rawind % raw_stride[axis]) / raw_stride[axis + 1];
+    rawind += (2*forward-1)*raw_stride[axis + 1];
+    if(axisind == forward*(shape[axis]-1))
+        rawind -= (2*forward-1)*raw_stride[axis];
+    return rawind;
+}
+/**
+* @brief roll ind for unknow positiveness by using rollindex
+*/
+template <typename T, int D>
+size_t rollind(size_t rawind, int ax, const ndarray<T, D> &arr) {
+    if(ax < 0) {
+        return rollindex<false>(rawind, ax + D, arr.shape(), arr.raw_stride());
+    } else {
+        return rollindex<true>(rawind, ax, arr.shape(), arr.raw_stride());
+    }
+}
+
+template<class dtype>
 class matrix: public ndarray<dtype, 2> {
   public:
     using ndarray<dtype, 2>::operator=;
     using ndarray<dtype, 2>::ndarray;
-    void print() {
-        for(int i = 0; i < this->shape(0); i++) {
-            for(int j = 0; j < this->shape(1); j++) {
-                cout << (*this)(i, j) << '\t';
+    friend ostream& operator<<(ostream &os, const matrix &m) {
+        cout<< "Matrix: "<<m.shape(0)<<"x"<<m.shape(1)<<endl;
+        for(int i = 0; i < m.shape(0); i++) {
+            for(int j = 0; j < m.shape(1); j++) {
+                os << m(i, j) << '\t';
             }
-            cout << endl;
+            os << endl;
         }
+        return os;
     }
     /**
      * Matrix product (deprecated due to inefficiency)
--- test.cpp
+++ test.cpp
@@ -5,18 +5,18 @@ using namespace std;
 
 int main()
 {
-    ndarray<int, 2> arr{20,10};
-    auto coo=nditer<2>{20,10};
-    for(int i=0;i<arr.size();i++){
-        arr[i]=i;
+    int m=10000, n=1024;
+    matrix<int> arr{m, n};
+    arr = 1;
+    for (int i=1; i<m; i++){
+        for (int j=1; j<n; j++){
+//             arr[i*arr.stride(0)+j] += arr[(i-1)*arr.stride(0)+(j-1)] + arr[(i-1)*arr.stride(0)+j] + arr[i*arr.stride(0)+(j-1)];
+//             arr[i*n+j] += arr[(i-1)*n+(j-1)] + arr[(i-1)*n+j] + arr[i*n+(j-1)];
+//             arr[i*1024+j] += arr[(i-1)*1024+(j-1)] + arr[(i-1)*1024+j] + arr[i*1024+(j-1)];
+            arr(i, j) += arr((i-1), (j-1)) + arr((i-1), j) + arr(i, j-1);
+        }
     }
-    for(const auto k: arr){
-        cout<<k<<endl;
-    }
-    do{
-        coo.print();
-        cout<<arr[coo()]<<endl;
-    }while(++coo);
-    cout<<sizeof (arr)<<endl;
+    cout << arr(m-1, n-1)<<endl;
+    cout<<arr;
     return 0;
 }
